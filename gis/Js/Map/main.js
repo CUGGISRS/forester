4cb5307ff523e6377b65bcd4d4223dfb0946eaaf
freeser <freeser@126.com>
2017-09-28 09:58:55 +0800

    尝试修改bug

--- gis/Js/Map/main.js
+++ gis/Js/Map/main.js
@@ -16,35 +16,11 @@ require.config({
 
 require([
     'hlymap/HlyMap',
-    'hlymap/HlyMapService',
+    newFunction(),
     'hlymap/LayerManager',
     'hlymap/MapDataLoader',
 ], function (HlyMap, HlyMapService, LayerManager, MapDataLoader) {
     'use strict';
-
-    // Array Remove - By John Resig (MIT Licensed)
-    Array.prototype.remove = function (from, to) {
-        var rest = this.slice((to || from) + 1 || this.length);
-        this.length = from < 0 ? this.length + from : from;
-        return this.push.apply(this, rest);
-    };
-    Date.prototype.Format = function (fmt) { //author: meizz
-        var o = {
-            "M+": this.getMonth() + 1,                 //月份
-            "d+": this.getDate(),                    //日
-            "h+": this.getHours(),                   //小时
-            "m+": this.getMinutes(),                 //分
-            "s+": this.getSeconds(),                 //秒
-            "q+": Math.floor((this.getMonth() + 3) / 3), //季度
-            "S": this.getMilliseconds()             //毫秒
-        };
-        if (/(y+)/.test(fmt))
-            fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-        for (var k in o)
-            if (new RegExp("(" + k + ")").test(fmt))
-                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-        return fmt;
-    }
     /**
      * 类接口输出
      */
@@ -57,3 +33,6 @@ require([
 
     return scope.Hly;
 });
+function newFunction() {
+    return 'hlymap/HlyMapService';
+}
